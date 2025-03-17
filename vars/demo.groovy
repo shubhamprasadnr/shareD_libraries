4a628164e9ff53c7a7bde56900423c7481a1feb3
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-03-18 01:49:44 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -16,10 +16,8 @@ def gitCheckout(String branch, String repoUrl, String credentialsId = '') {
 
 def gitleaks() {
     sh '''
-        wget https://github.com/gitleaks/gitleaks/releases/download/v8.24.0/gitleaks_8.24.0_linux_x64.tar.gz
-        tar -xzvf gitleaks_8.24.0_linux_x64.tar.gz
-        chmod +x gitleaks
-        sudo mv gitleaks /usr/local/bin/
+      
+        sudo apt install gitleaks
     '''
 }
 def report(String reportName) {
