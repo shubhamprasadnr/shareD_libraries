2f3ad066aca2086cf7ded14e8def65e61ec1b93c
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-03-18 00:51:40 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,13 +1,24 @@
 def call() {
-  sh "date"
-  echo "hello shubham" 
+    cleanWs()
 }
 
-def test(name) {
-  sh "date"
-  echo "{$name}" 
+def call(String branch, String repoUrl, String credentialsId) {
+    git branch: branch, url: repoUrl, credentialsId: credentialsId
 }
 
-def code(url) {
-echo " {$url} "
+def call() {
+    sh '''
+        wget https://github.com/gitleaks/gitleaks/releases/download/v8.24.0/gitleaks_8.24.0_linux_x64.tar.gz
+        tar -xzvf gitleaks_8.24.0_linux_x64.tar.gz
+        chmod +x gitleaks
+        sudo mv gitleaks /usr/local/bin/
+    '''
 }
+def call(String reportName) {
+    sh "gitleaks detect -r ${reportName}"
+}
+
+def call(String reportName) {
+    archiveArtifacts artifacts: reportName, fingerprint: true
+}
+
