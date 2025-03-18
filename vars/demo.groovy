dd375520e7f4aef8b4bd9ae5e3b67c109ec38161
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-03-18 11:00:31 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -41,7 +41,8 @@ def owasp(){
    sh '''
    wget https://github.com/jeremylong/DependencyCheck/releases/download/v12.1.0/dependency-check-12.1.0-release.zip
    sudo apt install unzip
-   unzip dependency-check-12.1.0-release.zip
+   unzip -o dependency-check-12.1.0-release.zip
+
    
    '''
 }
