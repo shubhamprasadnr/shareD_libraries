aa5e49296e2ca7ce35c16541d85dd080963abb18
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-03-24 23:25:37 +0530

    Update prospector.groovy

--- vars/prospector.groovy
+++ vars/prospector.groovy
@@ -1,16 +1,3 @@
-// def call() {
-//     def VENV_DIR = "venv"
-
-//     sh """
-//         set -e
-//         ${VENV_DIR}/bin/pip install --upgrade pip setuptools wheel
-//         ${VENV_DIR}/bin/pip install pipreqs
-//         ${VENV_DIR}/bin/pip install prospector --no-deps
-//         ${VENV_DIR}/bin/prospector --profile strict --format html --output prospector.html
-
-//     """
-// }
-
 def call() {
     def VENV_DIR = "venv"
 
@@ -18,8 +5,21 @@ def call() {
         set -e
         ${VENV_DIR}/bin/pip install --upgrade pip setuptools wheel
         ${VENV_DIR}/bin/pip install pipreqs
-        ${VENV_DIR}/bin/pip install PyYAML  # Ensuring PyYAML is installed
         ${VENV_DIR}/bin/pip install prospector --no-deps
-        ${VENV_DIR}/bin/prospector --profile strict --format html --output prospector.html
+        ${VENV_DIR}/bin/prospector --format html --output prospector.html
+
     """
 }
+
+// def call() {
+//     def VENV_DIR = "venv"
+
+//     sh """
+//         set -e
+//         ${VENV_DIR}/bin/pip install --upgrade pip setuptools wheel
+//         ${VENV_DIR}/bin/pip install pipreqs
+//         ${VENV_DIR}/bin/pip install PyYAML  # Ensuring PyYAML is installed
+//         ${VENV_DIR}/bin/pip install prospector --no-deps
+//         ${VENV_DIR}/bin/prospector --profile strict --format html --output prospector.html
+//     """
+// }
