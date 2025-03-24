80eff8fe55aad1ce1c078e019c5d916db198f850
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-03-24 23:48:53 +0530

    Update prospector.groovy

--- vars/prospector.groovy
+++ vars/prospector.groovy
@@ -6,7 +6,7 @@ def call() {
         ${VENV_DIR}/bin/pip install --upgrade pip setuptools wheel
         ${VENV_DIR}/bin/pip install pipreqs
         ${VENV_DIR}/bin/pip install --upgrade setuptools
-        ${VENV_DIR}/bin/pip install pylint-django pylint-flask-extras
+        ${VENV_DIR}/bin/pip install pylint-django 
 
         ${VENV_DIR}/bin/pip install prospector 
         ${VENV_DIR}/bin/prospector --format html --output prospector.html
