e90016da0450b9cc67447e34ccb8c7731d49861e
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-03-24 23:19:23 +0530

    Update prospector.groovy

--- vars/prospector.groovy
+++ vars/prospector.groovy
@@ -6,6 +6,6 @@ def call() {
         ${VENV_DIR}/bin/pip install --upgrade pip setuptools wheel
         ${VENV_DIR}/bin/pip install pipreqs
         ${VENV_DIR}/bin/pip install prospector --no-deps
-        ${VENV_DIR}/bin/prospector --output-format full --profile strict || true
+        ${VENV_DIR}/bin/prospector --format HTML --output prospector.html full --profile strict || true
     """
 }
