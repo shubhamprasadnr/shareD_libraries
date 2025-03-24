a8d8e3592cfc68d9580926e2f4056ac369c2b5a6
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-03-24 23:41:38 +0530

    Update prospector.groovy

--- vars/prospector.groovy
+++ vars/prospector.groovy
@@ -2,12 +2,7 @@ def call() {
     def VENV_DIR = "venv"
 
     sh """
-        set -e
-        ${VENV_DIR}/bin/pip install --upgrade pip setuptools wheel
-        ${VENV_DIR}/bin/pip install pipreqs
-       ${VENV_DIR}/bin/pip install --upgrade setuptools
-       ${VENV_DIR}/bin/pip install git+https://github.com/jschaf/pylint-flask.git
-
+       
         ${VENV_DIR}/bin/pip install prospector 
         ${VENV_DIR}/bin/prospector --format html --output prospector.html
 
@@ -25,4 +20,10 @@ def call() {
 //         ${VENV_DIR}/bin/pip install prospector --no-deps
 //         ${VENV_DIR}/bin/prospector --profile strict --format html --output prospector.html
 //     """
+ set -e
+        ${VENV_DIR}/bin/pip install --upgrade pip setuptools wheel
+        ${VENV_DIR}/bin/pip install pipreqs
+       ${VENV_DIR}/bin/pip install --upgrade setuptools
+       ${VENV_DIR}/bin/pip install git+https://github.com/jschaf/pylint-flask.git
+
 // }
